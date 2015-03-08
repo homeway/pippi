7a5008012950617d0705828b2cb6bc5200aef76c
homeway <homeway.xue@gmail.com>
2015-03-08 12:22:37 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-erl -sync executable notification_center -pa deps/cowboy/ebin -pa deps/cowlib/ebin -pa deps/ranch/ebin -pa ebin
+erl -setcookie gdjy -name investor@10.0.11.68 -sync executable notification_center -pa deps/cowboy/ebin -pa deps/cowlib/ebin -pa deps/ranch/ebin -pa deps/jiffy/ebin -pa ebin
 # [application:start(A) || A <- [crypto, cowlib, ranch, cowboy, websocket]].
