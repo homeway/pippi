8e836c0e3801a7c1989f814244ec2a744872aec7
homeway <homeway.xue@gmail.com>
2015-03-09 16:35:42 +0800

    add uuid() to ss_utils

--- src/sms.erl
+++ src/sms.erl
@@ -10,46 +10,41 @@ send_multi(Contacts, Sms) ->
   send_multi(sendMulti, Contacts, Sms).
 
 send_multi(Mode, Contacts, Sms) when is_list(Contacts) ->
-  Batch = uuid(),
+  BatchId = ss_utils:uuid(),
   M = #{
     busi => Mode,
     data => #{
       list => [ #{tel => ss_convert:to_binary(To), msg => ss_convert:to_binary(Sms)} || To <- Contacts ],
-      batch => uuid()
+      batch => BatchId
     }
   },
-  erlang:display(M),
+  % erlang:display(M),
+  io:format("batchId: ~ts\n", [BatchId]),
   gate() ! {self(), jiffy:encode(M)},
-  receive {_From, Resp} ->
+  receive {_From, {Resp, BatchId}} ->
     io:format("~ts\n", [Resp]),
     case jiffy:decode(Resp, [return_maps]) of
-      #{<<"code">> := <<"200">>} -> {ok, Batch};
+      #{<<"code">> := <<"200">>} -> {ok, BatchId};
       Error -> {error, Error}
     end
   after 1000 ->
     io:format("timeout\n"),
     {error, timeout} end.
 
-status(Batch) ->
+status(BatchId) ->
   M = #{
     busi => findReports,
     data => #{
-      batch => Batch
+      batch => BatchId
     }
   },
   gate() ! {self(), jiffy:encode(M)},
-  receive {_From, Resp} ->
+  receive {_From, {Resp, BatchId}} ->
     io:format("~ts\n", [Resp]),
     case jiffy:decode(Resp, [return_maps]) of
-      #{<<"code">> := <<"200">>} -> {ok, Batch};
+      #{<<"code">> := <<"200">>} -> {ok, BatchId};
       Error -> {error, Error}
     end
   after 3000 ->
     io:format("timeout\n"),
     {error, timeout} end.
-
-uuid() ->
-    <<A:32, B:16, C:16, D:16, E:48>> = crypto:rand_bytes(16),
-    Str = io_lib:format("~8.16.0b-~4.16.0b-4~3.16.0b-~4.16.0b-~12.16.0b", 
-                        [A, B, C band 16#0fff, D band 16#3fff bor 16#8000, E]),
-    list_to_binary(Str).
