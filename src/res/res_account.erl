253b167d0abd81b4ba4e342c4ee98a48195cf55d
homeway <homeway.xue@gmail.com>
2015-03-09 20:11:00 +0800

    crud for account

--- src/res/res_account.erl
+++ src/res/res_account.erl
@@ -1,6 +1,7 @@
 %% -*- mode: nitrogen -*-
 -module(res_account).
--export([init_tables/0, insert/1, all/0, check_pass/2, to_test/0]).
+-export([init_tables/0, insert/1, all/0, get/1, delete/1, update/2, check_pass/2, to_test/0]).
+-export([generate_token/1, token_info/1, delete_token/1]).
 -include_lib("stdlib/include/qlc.hrl").
 
 -record(account, {name, email}).
@@ -14,7 +15,7 @@ to_test() ->
 %% {account, id(), #{id(), name(), email(), tel(), pass()}}
 %%
 %% table online
-%% {online, token(), #{account_id(), account_name()}}
+%% {online, token(), #{account_name()}}
 
 init_tables() ->
     mnesia:create_schema([node()]),
@@ -29,6 +30,31 @@ insert(Account) ->
     Item = {account, ss_utils:uuid(), Account},
     mnesia:transaction(fun() -> mnesia:write(Item) end).
 
+get(AccountName) ->
+    Cond = qlc:q([
+        {Id, Data} || {_, Id, Data} <- mnesia:table(account),
+        AccountName =:= maps:get(name, Data, undefined)]),
+    {atomic, R} = mnesia:transaction(fun()->qlc:e(Cond) end),
+    R.
+
+delete(Id) ->
+    Item = {account, Id},
+    {atomic, ok} = mnesia:transaction(mnesia:delete(Item)).
+
+update(Id, Account) ->
+    Cond = qlc:q([
+        Data || {_, Id0, Data} <- mnesia:table(account),
+        Id0 =:= Id]),
+    case mnesia:transaction(fun()->qlc:e(Cond) end) of
+        {atomic, []} ->
+            {error, not_exist};
+        {atomic, [{_, Id, Data0}|_]} ->
+            Item = {account, Id, maps:merge(Data0, Account)},
+            mnesia:transaction(mnesia:write(Item));
+        Error ->
+            Error
+    end.
+
 all() ->
     Tab = account,
     Cond = qlc:q([X || X <- mnesia:table(Tab)]),
@@ -42,3 +68,24 @@ check_pass(Name0, Pass0) ->
         Pass0=:=Pass1]),
     {atomic, R} = mnesia:transaction(fun() -> qlc:e(Cond) end),
     length(R) > 0.
+
+generate_token(AccountName) ->
+    Token = ss_utils:uuid(),
+    Item = {online, Token, AccountName},
+    {atomic, ok} = mnesia:transaction(fun() -> mnesia:write(Item) end),
+    Token.
+
+token_info(Token) ->
+    Cond = qlc:q([
+        AccountName || {_, Token0, AccountName} <- mnesia:table(online),
+        Token0 =:= Token]),
+    case mnesia:transaction(fun() -> qlc:e(Cond) end) of
+        {atomic, []} ->
+            {error, token_not_exist};
+        {atomic, [Name]} ->
+            {ok, Name}
+    end.
+
+delete_token(Token) ->
+    Item = {online, Token},
+    {atomic, ok} = mnesia:transaction(fun() -> mnesia:delete(Item) end).
