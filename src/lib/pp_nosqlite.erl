22af9a1a7d06a17a9f3c361c9b26d707fd0fc5f3
homeway <homeway.xue@gmail.com>
2015-03-12 08:31:12 +0800

    add update_logs within update method

--- src/lib/pp_nosqlite.erl
+++ src/lib/pp_nosqlite.erl
@@ -1,6 +1,6 @@
 -module(pp_nosqlite).
 -export([
-  create_schema/1, init/2, table/1,
+  create_schema/1, create_table/2, table/1,
   create/2, create/3, delete/2, update/3, get/2,
   find/3, find_all/3, find_key/3,
   all/1]).
@@ -10,6 +10,14 @@
 %%% we use mnesia to store nosqlite
 %%% 
 %%% {table(), key(), map()}
+%%%
+%%% table() - table name
+%%% key()   - item key in table
+%%% map()   - data store in table with map()
+%%%           data map include some property auto generated:
+%%%           created_at, lastmodified_at, update_logs
+%%%
+%%%
 %%% above is the data schema of all data schema
 %%%
 %%% you can use tuple module style
@@ -25,13 +33,13 @@
 create_schema(Tables) ->
   mnesia:create_schema([node()]),
   mnesia:start(),
-  [T:init() || T <- Tables],
+  [T:create_table() || T <- Tables],
   mnesia:stop().
 
-init(Tab, disc) -> init(Tab, ram_copies);
-init(Tab, disc_only) -> init(Tab, disc_only_copies);
-init(Tab, ram) -> init(Tab, ram_copies);
-init(Tab, CopyType) ->
+create_table(Tab, disc) -> create_table(Tab, ram_copies);
+create_table(Tab, disc_only) -> create_table(Tab, disc_only_copies);
+create_table(Tab, ram) -> create_table(Tab, ram_copies);
+create_table(Tab, CopyType) ->
   mnesia:create_table(Tab,
     [{CopyType, [node()]}, {attributes, [key, map]}]).
 
@@ -55,11 +63,31 @@ delete(Key, {?MODULE, Tab}) ->
   {atomic, ok} = mnesia:transaction(fun()-> mnesia:delete(Item) end), ok.
 
 %% update an old item() with key() and new property map()
-update(Key, Map, {?MODULE, Tab}) ->
-  Item = {Tab, Key, Map#{
-    lastmodified_at => pp:now_to_human()
-  }},
-  {atomic, ok} = mnesia:transaction(fun()->mnesia:write(Item) end), ok.
+update(Key, NewProp, {?MODULE, Tab}) ->
+  case get(Key, {?MODULE, Tab}) of
+    not_found -> not_found;
+    error -> error;
+    multi_records -> multi_records;
+    [_Key, Data] ->
+      NewItem = {Tab, Key, data_to_update(Data, NewProp)},
+      {atomic, ok} = mnesia:transaction(fun()->mnesia:write(NewItem) end),
+      ok
+  end.
+
+%% private method for update
+data_to_update(Data, NewProp) ->
+  OldProp = prop_to_update(Data, NewProp),
+  OldLog = maps:get(update_logs, Data, []),
+  UpdateTime = pp:now_to_human(),
+  NewLog = [[UpdateTime, OldProp, NewProp]|OldLog],
+  maps:merge(Data, NewProp#{
+    update_logs => NewLog,
+    lastmodified_at => UpdateTime}).
+prop_to_update(Data, NewProp) ->
+  L = maps:to_list(NewProp),
+  R = [{PropK, maps:get(PropK, Data, undefined)} || {PropK, _} <- L],
+  maps:from_list(R).
+
 
 %% got an item with key()
 %% not allowd duplicate key()
