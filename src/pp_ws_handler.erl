ef92140b828795468b8f41000ba5fd74ec58e521
homeway <homeway.xue@gmail.com>
2015-03-17 07:35:07 +0800

    add pp_ws_handler.erl

---
+++ src/pp_ws_handler.erl
@@ -0,0 +1,33 @@
+-module(pp_ws_handler).
+-behaviour(cowboy_websocket_handler).
+
+-export([init/3]).
+-export([websocket_init/3]).
+-export([websocket_handle/3]).
+-export([websocket_info/3]).
+-export([websocket_terminate/3]).
+
+init({tcp, http}, _Req, _Opts) ->
+  {upgrade, protocol, cowboy_websocket}.
+
+%% 连接时
+websocket_init(_TransportName, Req, _Opts) ->
+  {ok, Req, undefined_state}.
+
+websocket_handle({text, Msg}, Req, State) ->
+  Cmds = jiffy:decode(Msg, [return_maps]),
+  pp:display(Msg),
+  erlang:display({"client text req: ", [Cmds]}),
+  {reply, {text, jiffy:encode([processing])}, Req, State};
+
+websocket_handle(_Data, Req, State) ->
+  {ok, Req, State}.
+
+websocket_info({client, Msg}, Req, State) ->
+  {reply, {text, Msg}, Req, State};
+websocket_info(_Info, Req, State) ->
+  {ok, Req, State}.
+
+websocket_terminate(_Reason, _Req, _State) ->
+  ok.
+
