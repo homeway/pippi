6e4fb23c67c098bb3d58385970069d1024eea169
homeway <homeway.xue@gmail.com>
2015-03-17 07:36:31 +0800

    add states of offline and online into account

--- src/account.erl
+++ src/account.erl
@@ -1,48 +1,74 @@
+%% -*- mode: nitrogen -*-
 -module(account).
 -behaviour(gen_fsm).
--export([start_link/0]).
+-export([start/0]).
 
 %% gen_fsm callbacks
--export([init/1, login/2, logout/2, locked/2]).
+-export([init/1, offline/2, online/2]).
 -export([handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
--define(SERVER, ?MODULE).
+-define(ONLINE_TIMEOUT, 30000).
+-define(OFFLINE_TIMEOUT, 30000).
 
-start_link() ->
-    gen_fsm:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+start() ->
+    gen_fsm:start(?MODULE, [], []).
 
 init([]) ->
-    {ok, logout, #{token=>undefined, users=>Users}}.
+    erlang:display({init, now(), ok}),
+    {ok, offline, #{slots=>[]}, ?OFFLINE_TIMEOUT}.
 
-logout({login, From, User, Pass}, State) ->
-  case maps:get(Account, all(), undefined) =:= Pass of
-    true ->
-      From ! [login, ok]);
-    false ->
-      From ! [login, failed])
-  end.
+offline({login, From, #{<<"user">> := <<"adi">>, <<"pass">> := <<"123">>}}, #{slots:=SlotsOld}=State) ->
+    SlotsNew => [From|SlotsOld],
+    %% return the result for caller
+    From ! {login, ok},
+    erlang:display({login, now(), ok}),
 
-    {next_state, login, State, 3000};
-logout(lock, State) ->
-    {next_state, locked, State, 3000}.
+    {next_state, online, State#{user=> <<"adi">>, slots=> SlotsNew}, ?ONLINE_TIMEOUT};
 
-login(logout, State) ->
-    {next_state, logout, State};
-login({message, Msg}, State) ->
+offline({login, From, Auth}, State) ->
+    From ! {login, error, now(), Auth},
+    erlang:display({login, error, Auth}),
+    {next_state, offline, State, ?ONLINE_TIMEOUT};
+
+offline(timeout, State) ->
+    erlang:display({timeout, now(), to_exit}),
+    {stop, normal, State};
+
+offline(OfflineMsg, State) ->
+    erlang:display({offline_message, now(), OfflineMsg}),
+    {next_state, offline, State}.
+
+online({message, Msg}, State) ->
     erlang:display(Msg),
-    {next_state, login, State, 3000};
-login(timeout, State) ->
-    {next_state, logout, State}.
+    erlang:display(State),
+    {next_state, online, State, ?ONLINE_TIMEOUT};
 
-locked(unlock, State) ->
-    {next_state, logout, State}.
+online(logout, State) ->
+    {next_state, offline, State};
+
+online(timeout, State) ->
+    erlang:display({timeout, to_logout}),
+    {next_state, offline, State, ?OFFLINE_TIMEOUT};
+
+online(Other, State) ->
+    erlang:display(Other),
+    erlang:display(State),
+    {next_state, online, State, ?ONLINE_TIMEOUT}.
 
 handle_event(_Event, StateName, State) ->
     {next_state, StateName, State}.
+
 handle_sync_event(_Event, _From, StateName, State) ->
     Reply = ok,
     {reply, Reply, StateName, State}.
 
-handle_info(_Info, StateName, State) -> {next_state, StateName, State}.
-terminate(_Reason, _StateName, _State) ->  ok.
+handle_info(Info, StateName, State) ->
+    erlang:display({handle_info, Info}),
+    {next_state, StateName, State}.
+
+terminate(_Reason, _StateName, State) ->
+    erlang:display(exit),
+    erlang:display(State), ok.
+
 code_change(_OldVsn, StateName, State, _Extra) -> {ok, StateName, State}.
 
