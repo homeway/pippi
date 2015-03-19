9af5c45f3782b77768981da8a57610af9d37eda2
homeway <homeway.xue@gmail.com>
2015-03-19 10:51:13 +0800

    add pp_account_test

--- tests/src/service/pp_account_test.erl
+++ tests/src/service/pp_account_test.erl
@@ -1,3 +1,24 @@
 -module(pp_account_test).
 -include_lib("eunit/include/eunit.hrl").
 
+clear_msg() ->
+    receive _ -> clear_msg()
+    after 50 -> ok
+    end.
+
+got_msg() ->
+    receive Msg -> Msg
+    after 50 -> nothing
+    end.
+
+main_test() ->
+    A1 = pp_account:start(1000, 1000),
+    clear_msg(),
+
+    %% 错误信息的登录
+    A1:login("user", "123"),
+    ?assertMatch({login, _Ref, {error, _Msg}}, got_msg()),
+    A1:stop().
+
+
+
