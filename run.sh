cc5b9f4b62b64849a042cba58cfc3d3f845581f1
homeway <homeway.xue@gmail.com>
2015-03-22 13:46:42 +0800

    erl ebin deps/*/ebin

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-DEPS=$(ls -d deps/*/ebin |xargs)
+#DEPS=$(ls -d deps/*/ebin |xargs)
 NAME="investor@10.0.11.68"
 START="auto_run"
 COOKIE="gdjy"
-CMD="erl -pa $DEPS -pa ebin -setcookie $COOKIE -name $NAME -s $START"
+CMD="erl -pa ebin deps/*/ebin -setcookie $COOKIE -name $NAME -s $START"
 
 $CMD
