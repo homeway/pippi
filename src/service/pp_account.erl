80145dadd4314d08b43f717c260f783d90c378bf
homeway <homeway.xue@gmail.com>
2015-03-24 10:20:18 +0800

    change to sync_send_event in pp_account

--- src/service/pp_account.erl
+++ src/service/pp_account.erl
@@ -2,22 +2,28 @@
 -module(pp_account).
 -behaviour(gen_fsm).
 -export([start/0, start/2, stop/1]).
--export([account/1, login/3, notify/2]).
+-export([account_tuple/1, login/3, status/1]).
 
 %% gen_fsm callbacks
--export([init/1, offline/2, online/2]).
+-export([init/1, offline/2, offline/3, online/2, online/3]).
 -export([handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+
+-define(TIMEOUT, 1000).
 
 %% api
 start() -> start(3000, 3000).
 start(OnlineTimeout, OfflineTimeout) ->
-    {ok, Pid} = gen_fsm:start(?MODULE, [#{online_timeout=>OnlineTimeout, offline_timeout=>OfflineTimeout}], []),
+    {ok, Pid} = gen_fsm:start(?MODULE, [#{
+        online_timeout => OnlineTimeout,
+        offline_timeout => OfflineTimeout,
+        subscribes => #{}
+    }], []),
     {?MODULE, Pid}.
 
 stop({?MODULE, Pid}) ->
     gen_fsm:send_all_state_event(Pid, stop).
 
-account(Pid) ->
+account_tuple(Pid) ->
     {?MODULE, Pid}.
 
 login(User, Pass, {?MODULE, AccountPid}) ->
@@ -25,54 +31,33 @@ login(User, Pass, {?MODULE, AccountPid}) ->
         user => pp:to_binary(User),
         pass => pp:to_binary(Pass)
     },
-    Ref = make_ref(),
-    gen_fsm:send_event(AccountPid, {login, self(), Ref, Auth}), Ref.
+    gen_fsm:sync_send_event(AccountPid, {login, Auth}, ?TIMEOUT).
 
-notify(Msg, {?MODULE, AccountPid}) ->
-    Ref = make_ref(),
-    gen_fsm:send_event(AccountPid, {notify, self(), make_ref(), Msg}), Ref.
+status({?MODULE, Pid}) ->
+    gen_fsm:sync_send_all_state_event(Pid, status, ?TIMEOUT).
 
 %% gen_fsm callbacks
 init([#{offline_timeout:=OfflineTimeout}=Conf]) ->
-    {ok, offline, maps:merge(#{slots=>[]}, Conf), OfflineTimeout}.
+    {ok, offline, Conf, OfflineTimeout}.
 
-offline({login, From, Ref, #{user := <<"adi">>, pass := <<"123">>}}, #{slots:=SlotsOld}=State) ->
-    SlotsNew = [From|SlotsOld],
-    % confirm
-    From ! {login, Ref, ok},
-    % broad cast
-    [Pid ! {online, Ref, <<"adi">>} || Pid <- SlotsNew],
-    {next_state, online, State#{user=> <<"adi">>, slots=> SlotsNew}, maps:get(online_timeout, State)};
+%％ login when offline
+offline({login, #{user := <<"adi">>, pass := <<"123">>}}, _From, State) ->
+    {reply, ok, online, State#{user=> <<"adi">>}, maps:get(online_timeout, State)};
 
-offline({login, From, Ref, Auth}, State) ->
-    From ! {login, Ref, {error, Auth}},
-    {next_state, offline, State, maps:get(offline_timeout, State)};
+%% login when offline
+offline({login, _Auth}, _From, State) ->
+    {reply, {error, bad_user_or_password}, offline, State, maps:get(offline_timeout, State)}.
 
 %% offline session to exit
 offline(timeout, State) ->
     {stop, normal, State};
 
-%% send a notify to offline
-offline({notify, From, Ref, _OfflineMsg}, State) ->
-    % confirm
-    From ! {notify, Ref, {ok, offline}},
-    {next_state, offline, State};
-
 offline(_Event, State) ->
     {next_state, online, State, maps:get(offline_timeout, State)}.
 
-%% send a notify to online
-online({notify, From, Ref, Msg}, #{slots:=Slots}=State) ->
-    % confirm
-    From ! {notify, Ref, {ok, online}},
-    % broad cast
-    [Pid ! {notify, Ref, Msg} || Pid <- Slots],
-    {next_state, online, State, maps:get(online_timeout, State)};
-
 %% to logout
-online({logout,From, Ref}, State) ->
-    From ! {logout, Ref, ok},
-    {next_state, offline, State};
+online(logout, _From, State) ->
+    {reply, ok, offline, State}.
 
 %% online session timeout
 online(timeout, State) ->
@@ -84,10 +69,17 @@ online(_Event, State) ->
 %% stop action
 handle_event(stop, _StateName, State) ->
     {stop, normal, State};
+
+%% status
 handle_event(_Event, StateName, State) ->
     {next_state, StateName, State}.
 
-handle_sync_event(_Event, _From, StateName, State) -> {reply, ok, StateName, State}.
+handle_sync_event(status, _From, StateName, State) ->
+    {reply, {StateName, State}, StateName, State};
+
+handle_sync_event(_Event, _From, StateName, State) ->
+    {reply, no_this_action, StateName, State}.
+
 handle_info(_Info, StateName, State) -> {next_state, StateName, State}.
 terminate(_Reason, _StateName, _State) -> ok.
 code_change(_OldVsn, StateName, State, _Extra) -> {ok, StateName, State}.
--- tests/src/service/pp_account_test.erl
+++ tests/src/service/pp_account_test.erl
@@ -1,46 +1,31 @@
+%% -*- mode: nitrogen -*-
 -module(pp_account_test).
 -include_lib("eunit/include/eunit.hrl").
 
-clear_msg() ->
-    receive _ -> clear_msg()
-    after 50 -> ok
-    end.
+%% clear_msg() ->
+%%     receive _ -> clear_msg()
+%%     after 50 -> ok
+%%     end.
 
-got_msg() ->
-    receive Msg -> Msg
-    after 50 -> nothing
-    end.
+%% got_msg() ->
+%%     receive Msg -> Msg
+%%     after 50 -> nothing
+%%     end.
 
-main_test() ->
+login_test() ->
     %% start
     A1 = pp_account:start(1000, 1000),
 
-    %% 发送离线通知
-    clear_msg(),
-    A1:notify("hello"),
-    ?assertMatch({notify, _Ref, {ok, offline}}, got_msg()),
-
     %% 错误的登录
-    clear_msg(),
-    A1:login("user", "123"),
-    ?assertMatch({login, _Ref, {error, _Msg}}, got_msg()),
+    ?assertMatch({error, _Msg}, A1:login("user", "123")),
+
+    ?assertMatch({offline, _Status}, A1:status()),
 
     %% 正确的登录
-    clear_msg(),
-    A1:login("adi", "123"),
-    ?assertMatch({login, _Ref, ok}, got_msg()),
-    %% slots群发
-    ?assertMatch({online, _Ref, <<"adi">>}, got_msg()),
+    ?assertMatch(ok, A1:login("adi", "123")),
 
-    %% 发送在线通知
-    clear_msg(),
-    A1:notify("hello"),
-    ?assertMatch({notify, _Ref, {ok, online}}, got_msg()),
-    %% slots群发
-    ?assertMatch({notify, _Ref, "hello"}, got_msg()),
+    A1:status(),
+    ?assertMatch({online, #{user := <<"adi">>}}, A1:status()),
 
     %% exit
     A1:stop().
-
-
-
