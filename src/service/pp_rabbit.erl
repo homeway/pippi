f592d6783e1ad69bd18c9202009cee043f6b18ec
homeway <homeway.xue@gmail.com>
2015-04-19 15:39:04 +0800

    rpc client and server with amqp client

--- src/service/pp_rabbit.erl
+++ src/service/pp_rabbit.erl
@@ -17,6 +17,10 @@
 
 %% API
 -export([start/0, stop/0, start_link/0, rpc_call/2]).
+-export([reg_rpc_server/2, reg_rpc_server/4, unreg_rpc_server/1,
+    reg_rpc_client/1, unreg_rpc_client/1,
+    reg_auto_rpc_server/4, unreg_auto_rpc_server/1,
+    reg_auto_rpc_client/1, unreg_auto_rpc_client/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -24,6 +28,7 @@
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
+-define(AMQP_CLIENT_TEST_CONNECTION_TYPE, "network").
 -define(SERVER, ?MODULE).
 
 %%%===============================================================
@@ -40,81 +45,143 @@ stop() ->
     gen_server:cast(?SERVER, stop).
 
 init(_) ->
-    Network = #amqp_params_network{host="localhost"},
-    {ok, Conn} = amqp_connection:start(Network),
-
-    Queues = [],
-
-    List1 = [
-        {pp:to_binary(Q), amqp_rpc_client:start_link(Conn, Q)} ||
-        Q <- Queues
-    ],
-    {ok, #{connect => Conn, queues => maps:from_list(List1)}}.
+    Params = make_network_params([]),
+    {ok, Conn} = amqp_connection:start(Params),
+    case erlang:is_process_alive(Conn) of
+        false -> {stop, connect_to_amqp_failed};
+        true ->
+            link(Conn),
+            pp:display({amqp_connection, Conn}),
+            {ok, #{connect => Conn, clients => #{}, servers => #{}}}
+    end.
 
 %% register a rpc server:
 %% the params and returned data must compatible with jiffy json
 
 %% register an erlang rpc server
-reg_rpc_server(Queue, Func) when is_function(Func)->
-    ok.
-reg_rpc_server(Queue, M, F, Count) ->
-    ok.
-unreg_rpc_server(Queue) ->
-    ok.
-
-%% register all functions in module as rpc server
-reg_rpc_module(M) ->
-    ok.
-unreg_rpc_module(M) ->
-    ok.
+reg_rpc_server(Q, Func) when is_function(Func)->
+    gen_server:call(?SERVER, {reg_rpc_server, Q, Func}).
+reg_rpc_server(Q, M, F, Count) ->
+    gen_server:call(?SERVER, {reg_rpc_server, Q, fun M:F/Count}).
+unreg_rpc_server(Q) ->
+    gen_server:call(?SERVER, {unreg_rpc_server, Q}).
 
 %% register a temperory client
-reg_rpc_client(Queue) ->
-    ok.
-unreg_rpc_client(Queue) ->
-    ok.
+reg_rpc_client(Q) ->
+    gen_server:call(?SERVER, {reg_rpc_client, Q}).
+unreg_rpc_client(Q) ->
+    gen_server:call(?SERVER, {unreg_rpc_client, Q}).
 
 %% prepare to auto register rpc server by erlang
-reg_auto_rpc_server(Queue, M, F, Count) ->
+reg_auto_rpc_server(Q, M, F, Count) ->
     ok.
-unreg_auto_rpc_server(Queue) ->
-    ok.
-reg_auto_rpc_module(M) ->
-    ok.
-unreg_auto_rpc_module(M) ->
+unreg_auto_rpc_server(Q) ->
     ok.
 
 %% prepare to auto register rpc client
 %% the rpc service can be supply by none-erlang process
-reg_auto_rpc_client(Queue) ->
+reg_auto_rpc_client(Q) ->
     ok.
-unreg_auto_rpc_client(Queue) ->
+unreg_auto_rpc_client(Q) ->
     ok.
 
 %% rpc client call
-rpc_call(Queue, Params) ->
-    gen_server:call(?SERVER, {rpc_call, Queue, Params}).
+rpc_call(Q, Params) ->
+    gen_server:call(?SERVER, {rpc_call, Q, Params}).
 
 %%%===============================================================
 %%% gen_server callback
 %%%===============================================================
 
 handle_call(status, _F, S) -> {reply, S, S};
-handle_call({rpc_call, Q, P}, _F, #{connect:=Conn, queues:=Queues}=S) ->
-    case maps:get(pp:to_binary(Q), Queues, undefined) of
+
+handle_call({reg_rpc_server, Q, Func}, _F, #{connect:=Conn, servers:=Servers}=S) ->
+    RPCHandler = fun(X) ->
+        try jiffy:encode(apply(Func, jiffy:decode(X))) of
+            Res1 -> Res1
+        catch
+            _:_ -> <<"error">>
+        end
+    end,
+    Server = amqp_rpc_server:start_link(Conn, pp:to_binary(Q), RPCHandler),
+    erlang:display({reg_rpc_server, Q, Server}),
+    NewServers = maps:put(pp:to_binary(Q), Server, Servers),
+    {reply, ok, S#{servers=>NewServers}};
+
+handle_call({unreg_rpc_server, Q}, _F, #{servers:=Servers}=S) ->
+    case maps:get(pp:to_binary(Q), Servers, undefined) of
+        undefined -> {reply, undefiend_queue, S};
+        Server ->
+            unlink(Server),
+            amqp_rpc_server:stop(Server),
+            NewServers = maps:remove(pp:to_binary(Q), Servers),
+            {reply, ok, S#{servers=>NewServers}}
+    end;
+
+handle_call({reg_rpc_client, Q}, _F, #{connect:=Conn, clients:=Clients}=S) ->
+    Client = amqp_rpc_client:start_link(Conn, pp:to_binary(Q)),
+    erlang:display({reg_rpc_client, Q, Client}),
+    NewClients = maps:put(pp:to_binary(Q), Client, Clients),
+    {reply, ok, S#{clients=>NewClients}};
+
+handle_call({unreg_rpc_client, Q}, _F, #{clients:=Clients}=S) ->
+    case maps:get(pp:to_binary(Q), Clients, undefined) of
+        undefined -> {reply, undefiend_queue, S};
+        Client ->
+            unlink(Client),
+            amqp_rpc_client:stop(Client),
+            NewClients = maps:remove(pp:to_binary(Q), Clients),
+            {reply, ok, S#{clients=>NewClients}}
+    end;
+
+handle_call({rpc_call, Q, P}, _F, #{clients:=Clients}=S) when is_binary(P)->
+    pp:display(Clients),
+    case maps:get(pp:to_binary(Q), Clients, undefined) of
         undefined -> {reply, undefined, S};
-        Client -> {reply, amqp_rpc_client:call(Client, P), S}
+        Client ->
+            {reply, amqp_rpc_client:call(Client, P), S}
+    end;
+handle_call({rpc_call, Q, P}, F, S) ->
+    try jiffy:encode(P) of
+        P2 -> handle_call({rpc_call, Q, P2}, F, S)
+    catch
+        _:_ -> {reply, <<"[error, invalid_param]">>, S}
     end.
+
 
 handle_cast(stop, State) -> {stop, normal, State};
 handle_cast(_Msg, State) -> {noreply, State}.
 
 handle_info(_Info, State) -> {noreply, State}.
 
-terminate(_Reason, #{connect:=Conn, queues:=Queues }) ->
-    List1 = maps:to_list(Queues),
-    [amqp_rpc_client:stop(Client) || {_Q, Client} <- List1],
+terminate(_Reason, #{connect:=Conn, clients:=Clients, servers:=Servers}=S) ->
+    %% %% close servers
+    %% lists:foreach(fun({_Q, Srv}) ->
+    %%     unlink(Srv),
+    %%     amqp_rpc_server:close(Srv)
+    %% end, maps:to_list(Servers)),
+    %% %% close clients
+    %% lists:foreach(fun({_Q, C}) ->
+    %%     unlink(C),
+    %%     amqp_rpc_client:close(C)
+    %% end, maps:to_list(Clients)),
+    %% %% close connection
     amqp_connection:close(Conn),
     ok.
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+%%% private
+%%%
+
+%% Note: not all amqp_params_network fields supported.
+make_network_params(Props) ->
+    Pgv = fun (Key, Default) ->
+                  proplists:get_value(Key, Props, Default)
+          end,
+    #amqp_params_network{username     = Pgv(username, <<"guest">>),
+                         password     = Pgv(password, <<"guest">>),
+                         virtual_host = Pgv(virtual_host, <<"/">>),
+                         channel_max  = Pgv(channel_max, 0),
+                         ssl_options  = Pgv(ssl_options, none),
+                         host         = Pgv(host, "localhost")}.
--- tests/src/service/pp_rabbit_test.erl
+++ tests/src/service/pp_rabbit_test.erl
@@ -4,11 +4,38 @@
 
 main_test() ->
     Add1 = fun(A, B) -> A + B end,
-    Add2 = fun(A, B) -> A + B + 1 end,
-    Queues = [add1, add2],
     pp_rabbit:start(),
 
-    % ?assertMatch(<<"3">>, pp_rabbit:call(add, [1,2])),
+    %% common rpc client/server by erlang
+    pp_rabbit:reg_rpc_server(add1, Add1),
+    pp_rabbit:reg_rpc_client(add1),
+    ?assertMatch(<<"3">>, pp_rabbit:rpc_call(add1, [1,2])),
+
+    %% rpc call by json
+    ?assertMatch(<<"3">>, pp_rabbit:rpc_call(add1, <<"[1,2]">>)),
+
+    %% exception in rpc client call
+    ?assertMatch(<<"error">>, pp_rabbit:rpc_call(add1, <<"\"[1,2]\"">>)),
+    %% invalid rpc param call
+    ?assertMatch(<<"[error,", Rest/binary>>, pp_rabbit:rpc_call(add1, {1, 2, 1})),
+
+    %% rpc pipeline queue
+    Add2 = fun(A, B) -> A + B + 1 end,
+    pp_rabbit:reg_rpc_server(add1, Add2),
+
+    ?assertMatch(<<"3">>, pp_rabbit:rpc_call(add1, [1,2])),
+    ?assertMatch(<<"4">>, pp_rabbit:rpc_call(add1, [1,2])),
+
+    pp_rabbit:unreg_rpc_client(add1),
+    ?assertMatch(undefined, pp_rabbit:rpc_call(add1, [1,2])),
+    pp_rabbit:reg_rpc_client(add1),
+
+    %% utf8
+    Add3 = fun(A, B) -> <<"结果是: "/utf8, (pp:to_binary(A + B))/binary>> end,
+    pp_rabbit:reg_rpc_server(add3, Add3),
+    pp_rabbit:reg_rpc_client(add3),
+    R = pp_rabbit:rpc_call(add3, [1,2]),
+    ?assertMatch(<<"结果是: 3"/utf8>>, jiffy:decode(R)),
 
     pp_rabbit:stop(),
     ok.
